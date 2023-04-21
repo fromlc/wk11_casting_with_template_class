32f9cc73f80e6605a408c8ea6048100ff90d5807
L Carver <wifihum@gmail.com>
2023-04-21 12:52:13 -0700

    Add project files.

---
+++ source/Fruit.h
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------------
+// Fruit.h : declaration and definition file for
+//      - base class Fruit
+//      - derived classes Orange, Banana
+//------------------------------------------------------------------------------
+#pragma once
+
+#define LC_DEBUG
+#ifdef LC_DEBUG
+#include <iostream>
+using std::cout;
+#endif
+
+#include <string>
+using std::string;
+
+//------------------------------------------------------------------------------
+// constants
+//------------------------------------------------------------------------------
+constexpr int _FRUIT = 0;
+constexpr int ORANGE = 1;
+constexpr int BANANA = 2;
+
+//------------------------------------------------------------------------------
+// Fruit
+//------------------------------------------------------------------------------
+class Fruit {
+public:
+    int fruitID;
+    string color;
+    int calories;
+
+    // constructors
+    Fruit(int _fruitID, string _color, int _calories) {
+        fruitID = _fruitID;
+        color = _color;
+        calories = _calories;
+    }
+
+    Fruit() : Fruit(_FRUIT, "none", 0) { }
+
+#ifdef LC_DEBUG
+    ~Fruit() { cout << "Fruit destructor\n"; }
+#endif
+};
+
+//------------------------------------------------------------------------------
+// Orange
+//------------------------------------------------------------------------------
+class Orange : public Fruit {
+public:
+    Orange() : Fruit::Fruit(ORANGE, "orange", 50) { }
+#ifdef LC_DEBUG
+    ~Orange() { cout << "Orange destructor\n"; }
+#endif
+};
+
+//------------------------------------------------------------------------------
+// Banana
+//------------------------------------------------------------------------------
+class Banana : public Fruit {
+public:
+    Banana() : Fruit::Fruit(BANANA, "yellow", 90) { }
+#ifdef LC_DEBUG
+    ~Banana() { cout << "Banana destructor\n"; }
+#endif
+};
---
+++ source/fruit_vector.cpp
@@ -0,0 +1,105 @@
+//------------------------------------------------------------------------------
+// fruit_vector.cpp 
+//
+// Demos vector that
+//	- stores pointers to base class Fruit instances
+//	- cast pointers to process derived class Orange and Banana instances
+//------------------------------------------------------------------------------
+#include "Fruit.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+//------------------------------------------------------------------------------
+// using symbols
+//------------------------------------------------------------------------------
+using std::cout;
+using std::string;
+using std::vector;
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+vector<Fruit*>* getFruitVector();
+void displayFruits(vector<Fruit*>*);
+void deleteFruits(vector<Fruit*>*);
+
+//------------------------------------------------------------------------------
+// entry point
+//------------------------------------------------------------------------------
+int main() {
+
+	vector<Fruit*>* pVFruits = getFruitVector();
+
+	displayFruits(pVFruits);
+
+	deleteFruits(pVFruits);
+
+	// very inefficient but convenient way to keep app window open
+	system("pause");
+
+	return 0;
+}
+
+//------------------------------------------------------------------------------
+// - instantiate classes derived from Fruit
+// - store instance pointers in new vector<Fruit>
+// - return pointer to new vector<Fruit>
+//------------------------------------------------------------------------------
+vector<Fruit*>* getFruitVector() {
+
+	// tell C++ we'll store pointers to base class instances
+	vector<Fruit*>* pV = new vector<Fruit*>;
+
+	// store pointers to derived class instances instead
+	// there are different ways to do this...
+	Orange* pOrange = new Orange;
+	pV->push_back(pOrange);
+	pV->push_back(new Orange);
+	pV->push_back(new Banana);
+	pV->push_back((Banana*) new Banana);
+
+	// we can store base class pointers too
+	pV->push_back(new Fruit);
+
+	return pV;
+}
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+void displayFruits(vector<Fruit*>* pV) {
+
+	int elementNum = 0;
+
+	// traverse vector of pointers with range-based for loop
+	for (auto p : *pV) {
+
+		cout << "Vector element " << elementNum++ << ": ";
+
+		if (p->fruitID == ORANGE) {
+			cout << "Oranges are ";
+		}
+		else if (p->fruitID == BANANA) {
+			cout << "Bananas are ";
+		}
+		else if (p->fruitID == _FRUIT) {
+			cout << "Fruits have ";
+		}
+		cout << p->color << '\n';
+	}
+	cout << '\n';
+}
+
+//------------------------------------------------------------------------------
+// delete vector elements
+//------------------------------------------------------------------------------
+void deleteFruits(vector<Fruit*>* pV) {
+
+	// traverse vector of pointers with iterator this time
+	for (auto it = pV->begin(); it != pV->end(); ++it) {
+		delete (*it);
+	}
+}
+
